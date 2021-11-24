f729cb4d83111a9c34d7d5e821c7bd67a739f949
robLittiere <robinl.95@orange.fr>
2021-11-24 14:58:24 +0100

    Getone Product

--- src/main/java/com/cergy/javaav/Services/ProductDao.java
+++ src/main/java/com/cergy/javaav/Services/ProductDao.java
@@ -1,13 +1,12 @@
 package com.cergy.javaav.Services;
-
 import com.cergy.javaav.models.Product;
-import com.cergy.javaav.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -22,5 +21,48 @@ public class ProductDao {
         String query = "SELECT * FROM product";
         list = template.query(query, BeanPropertyRowMapper.newInstance(Product.class));
         return list;
+    }
+
+    public Product getProductById(int id) {
+        String query = "SELECT * FROM product WHERE id = ?";
+        Product product = template.queryForObject(query, BeanPropertyRowMapper.newInstance(Product.class), id);
+        return product;
+    }
+
+    public String deleteProductById(int id) {
+
+        String query = "DELETE FROM product WHERE id = ?";
+        int result = template.update(query, id);
+        if(result == 1){
+            return "Product deleted";
+        }
+        return "Product not found";
+    }
+
+    public String addProduct(Product product) {
+        // Ajouter la date au produit
+        Date date = new Date();
+        product.setCreatedAt(date);
+
+        // Création de la query MySQL
+        String query = "INSERT INTO product (type, rating, name, createdAt, categoryId) values (?, ?, ?, ?, ?)";
+        int result = template.update(query, product.getType(), product.getRating(), product.getName(), product.getCreatedAt(), product.getCategoryId());
+        // Si la requète est réussit :
+        if(result == 1){
+            return "Product added";
+        }
+        return "Error";
+    }
+
+    public String updateProduct(Product product, int id) {
+        String query = "UPDATE product SET type = ?, rating = ?, name = ?, categoryId = ?  WHERE id = ?";
+        int result = template.update(query, product.getType(), product.getRating(), product.getName(), product.getCategoryId(), id);
+        // Si la requète est réussit :
+        if(result == 1){
+            return "Product added";
+        }
+        return "Error";
+
+
     }
 }
--- src/main/java/com/cergy/javaav/controllers/ProductController.java
+++ src/main/java/com/cergy/javaav/controllers/ProductController.java
@@ -4,10 +4,9 @@ import com.cergy.javaav.Services.ProductDao;
 import com.cergy.javaav.models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -21,6 +20,29 @@ public class ProductController {
     public List<Product> index(){
         List<Product> list = productDao.listAll();
         return list;
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public String addProduct(@RequestBody Product product){
+        return productDao.addProduct(product);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public String updateProduct(@PathVariable(value="id") int id, @RequestBody Product product){
+        return productDao.updateProduct(product, id);
+        //return productDao.updateProduct(product);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Product getById(@PathVariable(value="id") int id){
+        Product product =  productDao.getProductById(id);
+        return product;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public String deleteById(@PathVariable(value="id") int id){
+        String validation = productDao.deleteProductById(id);
+        return validation;
     }
 
 
